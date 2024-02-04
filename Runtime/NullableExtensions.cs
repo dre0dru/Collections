4f26a7b7eda232055ed85d6271172348638de05b
Dre0Dru <dre0druofficial@gmail.com>
2024-02-04 21:15:34 +0300

    feat: added nullable extensions

---
+++ Runtime/NullableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Dre0Dru.Collections
+{
+    public static class NullableExtensions
+    {
+        public static bool TryGet<T>(T? nullable, out T value)
+            where T: struct
+        {
+            value = default;
+            
+            if (nullable.HasValue)
+            {
+                value = nullable.Value;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

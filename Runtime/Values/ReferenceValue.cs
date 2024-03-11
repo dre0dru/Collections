be4ea85066d3b92ba79206b28d1501d1c3050a8e
Dre0Dru <dre0druofficial@gmail.com>
2024-03-11 21:47:15 +0300

    feat: added `Values` module
    chore: folders structure cleanup

---
+++ Runtime/Values/ReferenceValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dre0Dru.Collections
+{
+    [Serializable]
+    public class ReferenceValue<TValue>
+    {
+        [SerializeField]
+        private TValue _value;
+
+        public TValue Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public ReferenceValue() : this(default)
+        {
+            
+        }
+        
+        public ReferenceValue(TValue value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator TValue(ReferenceValue<TValue> referenceValue)
+        {
+            return referenceValue._value;
+        }
+    }
+}
---
+++ Runtime/Values/ValuesExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dre0Dru.Collections
+{
+    public static class ValuesExtensions
+    {
+        public static bool TryToConsume<T>(this ConsumableValue<T> consumableValue, out T value)
+        {
+            value = default;
+
+            if (!consumableValue.IsConsumed)
+            {
+                consumableValue.IsConsumed = true;
+                value = consumableValue;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool HasValueChanged<T>(this ValueChange<T> valueChange)
+            where T : IEquatable<T>
+        {
+            return !EqualityComparer<T>.Default.Equals(valueChange.PreviousValue, valueChange.CurrentValue);
+        }
+    }
+}

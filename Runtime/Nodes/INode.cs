b2c33a6b3ec283364e46fce919fde74b2f6e28a5
Dre0Dru <dre0druofficial@gmail.com>
2024-03-19 22:09:14 +0300

    feat: removed node id

--- Runtime/Nodes/INode.cs
+++ Runtime/Nodes/INode.cs
@@ -4,10 +4,6 @@ namespace Dre0Dru.Collections
 {
     public interface INode<TNode> : IEnumerable<TNode>
     {
-        NodeId Id { get; }
-        
-        void SetId(NodeId id);
-
         void AddChild(TNode child);
     }
 }
--- Runtime/Nodes/Node.cs
+++ Runtime/Nodes/Node.cs
@@ -9,67 +9,27 @@ namespace Dre0Dru.Collections
     public class Node<TNode> : INode<TNode>
         where TNode : INode<TNode>
     {
-        [SerializeField]
-        private NodeId _id;
-
         [SerializeReference]
         private List<TNode> _children;
 
         protected IList<TNode> Children => _children;
 
-        public NodeId Id => _id;
-
-        public Node() : this(NodeId.Default, new List<TNode>())
+        public Node() : this(new List<TNode>())
         {
         }
 
-        public Node(NodeId id) : this(id, new List<TNode>())
+        public Node(params TNode[] children) : this(new List<TNode>(children))
         {
         }
 
-        public Node(List<TNode> children) : this(NodeId.Default, children)
+        public Node(List<TNode> children)
         {
-        }
-        
-        public Node(params TNode[] children) : this(NodeId.Default, new List<TNode>(children))
-        {
-        }
-
-        public Node(NodeId id, params TNode[] children) : this(id, new List<TNode>(children))
-        {
-        }
-        
-        public Node(NodeId id, List<TNode> children)
-        {
-            _id = id;
             _children = children;
-        }
-
-        public void SetId(NodeId id)
-        {
-            _id = id;
-
-            var childrenDepth = _id.Depth + 1;
-
-            for (var i = 0; i < Children.Count; i++)
-            {
-                var child = Children[i];
-                child.SetId(new NodeId()
-                {
-                    Depth = childrenDepth,
-                    Index = i
-                });
-            }
         }
 
         public void AddChild(TNode child)
         {
             _children.Add(child);
-            child.SetId(new NodeId()
-            {
-                Depth = _id.Depth + 1,
-                Index = _children.Count - 1,
-            });
         }
 
         public IEnumerator<TNode> GetEnumerator()
